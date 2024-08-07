f13a636e4d6be9381dfdcf572f5dfbfe6e011520
SidZRed <siddharthrr@iisc.ac.in>
2024-08-08 00:02:30 +0530

    Add main.py

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
-import sys
+# main.py
+
+import argparse
 from tokenizer import Tokenizer
 from parser import Parser
-from intermediate import Document
 from html_renderer import HTML_Renderer
 from pdf_renderer import PDF_Renderer
-import argparse 
 
 def main():
-    parser = argparse.ArgumentParser(description="Process a techx file to render it as a HTML and PDF file.")
-    parser.add_argument('input_file', type=str, help="file path of a .techx file")
+    parser = argparse.ArgumentParser(description="Convert .techx file to HTML or PDF.")
+    parser.add_argument("input", help="Input .techx file")
+    parser.add_argument("--output-html", help="Output HTML file")
+    parser.add_argument("--output-pdf", help="Output PDF file")
     args = parser.parse_args()
 
-    input_file = args.input_file    
-    with open(input_file, 'r') as f:
-        text = f.read()
+    with open(args.input, 'r') as file:
+        text = file.read()
 
     tokenizer = Tokenizer(text)
     tokens = tokenizer.get_tokens()
 
     parser = Parser(tokens)
-    parsed_doc = parser.parse()
+    document = parser.parse()
 
-    html_renderer = HTML_Renderer(parsed_doc)
-    html_output = html_renderer.render()
-    with open('tech_doc.html', 'w') as file:
-        file.write(html_output)
-    
-    pdf_renderer = PDF_Renderer(parsed_doc, "tech_doc.pdf")
-    pdf_renderer.render()
+    if args.output_html:
+        html_renderer = HTML_Renderer(document)
+        html_content = html_renderer.render()
+        with open(args.output_html, 'w') as html_file:
+            html_file.write(html_content)
 
+    if args.output_pdf:
+        pdf_renderer = PDF_Renderer(document, args.output_pdf)
+        pdf_renderer.render()
 
 if __name__ == "__main__":
     main()
