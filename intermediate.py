47f1be6ab3203f76c8b0fca7ef972e22996113e7
SidZRed <siddharthrr@iisc.ac.in>
2024-08-08 00:02:20 +0530

    Modify structure

--- intermediate.py
+++ intermediate.py
@@ -1,24 +1,20 @@
+# intermediate.py
+
 class Node:
-    def __init__(self, type, content=None):
-        self.type = type
-        if content:
-            self.content = content
+    def __init__(self, node_type, content=None):
+        self.type = node_type
+        self.content = content or []
         self.children = []
 
-    def add_child(self, child):
-        self.children.append(child)
-    
-    def __repr__(self):
-        return f'Node({self.type}, {self.content}, {self.children})'
-
+    def add_child(self, child_node):
+        self.children.append(child_node)
 
 class Document:
     def __init__(self):
         self.root = Node('root')
-    
-    def add_section(self, title):
-        section = Node('section', title)
-        self.root.add_child(section)
-    
-    def __repr__(self):
-        return f'Document({self.root})'
+
+    def add_section(self, section_node):
+        self.root.add_child(section_node)
+
+    def __iter__(self):
+        return iter(self.root.children)  # Make Document iterable by iterating over root's children
