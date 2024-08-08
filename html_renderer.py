ad553747f49ced434cacb6b9ecad0ab343e3199d
SidZRed <siddharthrr@iisc.ac.in>
2024-08-08 11:21:24 +0530

    Style changes

--- html_renderer.py
+++ html_renderer.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class HTML_Renderer:
     def __init__(self, document):
         self.document = document
@@ -8,9 +10,20 @@ class HTML_Renderer:
         elif node.type == "author":
             return f"<p><strong>Author:</strong> {node.content[0]}</p>\n"
         elif node.type == "date":
+            if (len(node.content) == 0):
+                date = date.today()
+                return f"<p><strong>Date:</strong> {date}</p>\n"
             return f"<p><strong>Date:</strong> {node.content[0]}</p>\n"
         elif node.type == "section":
             return f"<h2>{node.content[0]}</h2>\n"
+        elif node.type == "code":
+            language = node.content.split(':')[0]
+            code = node.content.split(':')[1]
+            return f"<pre><code class=\"language-{language}\">{code}</code></pre>\n"
+        elif node.type == "api":
+            return f"<p><strong>API:</strong> {node.content[0]}</p>\n"
+        elif node.type == "changelog":
+            return f"<p><strong>Changelog:</strong> {node.content[0]}</p>\n"
         elif node.type == "text":
             return f"<p>{node.content}</p>\n"
         else:
