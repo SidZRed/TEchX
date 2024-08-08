6ffa2890ba80d31cff15a1a2eb91a9e8df3b65a2
SidZRed <siddharthrr@iisc.ac.in>
2024-08-08 10:45:31 +0530

    Slight modifications in intermediate structure

--- intermediate.py
+++ intermediate.py
@@ -1,9 +1,10 @@
-# intermediate.py
-
 class Node:
     def __init__(self, node_type, content=None):
         self.type = node_type
-        self.content = content or []
+        if content:
+            self.content = content
+        else:
+            self.content = []
         self.children = []
 
     def add_child(self, child_node):
@@ -17,4 +18,4 @@ class Document:
         self.root.add_child(section_node)
 
     def __iter__(self):
-        return iter(self.root.children)  # Make Document iterable by iterating over root's children
+        return iter(self.root.children)  
