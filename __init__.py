b594a100d4db8ec94d6d5c56ff9d040222c23b10
SidZRed <siddharthrr@iisc.ac.in>
2024-08-08 15:28:54 +0530

    Change project structure

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .tokenizer import Tokenizer
-from .parser import Parser
-from .intermediate import Document, Node
-from .html_renderer import HTML_Renderer
-from .pdf_renderer import PDF_Renderer
